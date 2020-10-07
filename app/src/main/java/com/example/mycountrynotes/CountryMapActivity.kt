f8cfaf607c88992cea696d6fdcb8155e40dddc5f
Zpaulis <paulis@zabarovski.lv>
2020-10-07 20:11:31 +0300

    Note CRUD working v3.3 Menu-back for detail added v3.4 different viewHolders added working v3.6 Photo from Gallerie to note working v4.0 Link to browser working, intent filter link added v4.1 Map View for Coutries working v5.0

--- app/src/main/java/com/example/mycountrynotes/CountryMapActivity.kt
+++ app/src/main/java/com/example/mycountrynotes/CountryMapActivity.kt
@@ -2,6 +2,7 @@ package com.example.mycountrynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import com.example.mycountrynotes.main.CountryItemRecyclerAdapter.Companion.pos
 import com.example.mycountrynotes.main.MainActivity.Companion.infos
@@ -12,14 +13,14 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 
-val coord = infos[pos].latlng
-val countryMarker : LatLng = LatLng(coord[0],coord[1])
 class CountryMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
-
     private lateinit var mMap: GoogleMap
+    val coord = infos[pos].latlng
+    val countryMarker : LatLng = LatLng(coord[0],coord[1])
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         Toast.makeText(this, coord.toString(), Toast.LENGTH_LONG).show()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_country_map)
@@ -32,5 +33,16 @@ class CountryMapActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         mMap.addMarker(MarkerOptions().position(countryMarker).title(infos[pos].name))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(countryMarker))
+    }
+    // Menu- back
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.getItemId()) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
